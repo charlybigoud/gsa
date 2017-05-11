f2953c25f6c077bf0a9f902f772039d3eda7a4cc
Charles Antoine Noury <charles_antoine.noury@univ-bpclermont.fr>
2017-05-11 11:51:41 +0200

    [simulated_annealing] moved some function in cpp

--- src/simulated_annealing.cpp
+++ src/simulated_annealing.cpp
@@ -1,1 +1,34 @@
 #include "simulated_annealing.hpp"
+
+SimulatedAnnealing::SimulatedAnnealing(double start_tmp, double stop_tmp, int it_max, double min_e
+    //, double tol = 0.1
+) :
+  start_temperature(start_tmp)
+, stop_temperature(stop_tmp)
+, iterations_per_temperature(it_max)
+, min_energy(min_e)
+// , acceptation_tolerance(tol)
+{};
+
+double SimulatedAnnealing::random_value()
+{
+    return double(std::rand()) / double(RAND_MAX);
+}
+
+bool SimulatedAnnealing::metropolisCritieria(const double delta_e, const double temperature)
+{
+    if (delta_e <= 0)
+        return true;
+    else if (random_value() <= std::exp(-delta_e / temperature) )
+        return true;
+    else
+        return false;
+}
+
+double SimulatedAnnealing::temperature() const
+{
+    // assert(current_it >= iterations_per_temperature);
+    // return ( double(current_it) / double(iterations_per_temperature)) * current_temperature;
+
+    return 0.99 * current_temperature;
+}
--- src/simulated_annealing.hpp
+++ src/simulated_annealing.hpp
@@ -30,102 +30,69 @@ struct SimulatedAnnealing
     int current_it;
     double current_energy;
 
-    double random_value()
-    {
-        return double(std::rand()) / double(RAND_MAX);
-    }
+    SimulatedAnnealing(double start_tmp = 1e3, double stop_tmp = 0.0, int it_max = 1000, double min_e = 0.0);
 
-    SimulatedAnnealing(double start_tmp = 1e3, double stop_tmp = 0.0, int it_max = 1000, double min_e = 0.0
-        //, double tol = 0.1
-    ):
-      start_temperature(start_tmp)
-    , stop_temperature(stop_tmp)
-    , iterations_per_temperature(it_max)
-    , min_energy(min_e)
-    // , acceptation_tolerance(tol)
-    {};
+    double random_value();
 
-    bool metropolisCritieria(const double delta_e, const double temperature)
-    {
-        if (delta_e <= 0)
-            return true;
-        else if (random_value() <= std::exp(-delta_e / temperature) )
-            return true;
-        else
-            return false;
-    }
+    bool metropolisCritieria(const double delta_e, const double temperature);
 
-
-
-
-////gives new tempratures
-    // double temperature() const
-    // {
-    //     assert(current_it >= iterations_per_temperature);
-
-    //     return ( double(current_it) / double(iterations_per_temperature)) * current_temperature;
-    // };
-
-    double temperature() const
-    {
-        return 0.99 * current_temperature;
-    };
-
-
-
+    double temperature() const;
 
     template<typename Energy, typename State, typename Generator>
-    void operator()(const Energy& energy, State& state, const Generator& generate)
+    void operator()(const Energy& energy, State& state, const Generator& generate);
+};
+
+template<typename Energy, typename State, typename Generator>
+void SimulatedAnnealing::operator()(const Energy& energy, State& state, const Generator& generate)
+{
+    //State to save in each temperature step
+    // State state_to_save;
+    // double energy_to_save;
+
+    //initiate system
+    current_it = 0;
+    current_temperature = start_temperature;
+    double previous_energy = energy(state);
+    current_energy = previous_energy;
+
+    bar();
+
+    while (
+        (current_temperature >= stop_temperature)
+    // or (current_energy >= min_energy)
+    )
     {
-        //State to save in each temperature step
-        // State state_to_save;
-        // double energy_to_save;
+        const State current_state = generate(state, *this);
+        current_energy = energy(current_state);
 
-        //initiate system
-        current_it = 0;
-        current_temperature = start_temperature;
-        double previous_energy = energy(state);
-        current_energy = previous_energy;
-
-        bar();
-
-        while (
-            (current_temperature >= stop_temperature)
-        // or (current_energy >= min_energy)
-        )
+        if (metropolisCritieria(current_energy - previous_energy, current_temperature))
         {
-            const State current_state = generate(state, *this);
-            current_energy = energy(current_state);
+            state = current_state;
+            previous_energy = current_energy;
 
-            if (metropolisCritieria(current_energy - previous_energy, current_temperature))
-            {
-                state = current_state;
-                previous_energy = current_energy;
+            // print<double, green>(*this);
+            print(*this, Accepted{});
 
-                // print<double, green>(*this);
-                print(*this, Accepted{});
-
-            }
-            else
-            {
-                // print<double, red>(*this);
-                // print(*this, Rejected{});
-            }
-
-            if (current_it > iterations_per_temperature)
-            {
-                // std::cout << iterations_per_temperature << "\n";
-                // std::cout << current_it << "\n";
-
-                current_temperature = temperature();
-                current_it = 0;
-            }
-            else
-            {
-               ++current_it;
-            }
+        }
+        else
+        {
+            // print<double, red>(*this);
+            // print(*this, Rejected{});
         }
 
-        final_print(*this);
+        if (current_it > iterations_per_temperature)
+        {
+            // std::cout << iterations_per_temperature << "\n";
+            // std::cout << current_it << "\n";
+
+            current_temperature = temperature();
+            current_it = 0;
+        }
+        else
+        {
+           ++current_it;
+        }
     }
-};
+
+    final_print(*this);
+}
